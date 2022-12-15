8cb7b3443263d93c984fb96291dbea3f48943df7
lthoreson <loganthoreson@gmail.com>
2022-12-15 10:26:01 -0600

    initial commit

---
+++ src/main/java/net/yorksolutions/budgetbe/BudgetBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.budgetbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetBeApplication.class, args);
+    }
+
+}
